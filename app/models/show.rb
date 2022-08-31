33b5ad463f27ef1b890dc5627b338539b3f4cd6a
Mwenje <mwenje24@gmail.com>
2022-08-31 08:11:17 +0300

    completed assignment

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,9 @@
 class Show < ActiveRecord::Base
-  
+  has_many :charaters
+  belongs_to :network
+  has_many :actors, through: :characters
+
+  def actors_list
+    self.actors.map {|actor| "#{actor.first_name} #{actor.last_name}"}
+  end
 end
